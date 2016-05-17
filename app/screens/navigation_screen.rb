79aee6c99790e3f99b402e7fc435e5ed6ba4c7b2
xdite <xuite.joke@gmail.com>
2016-05-18 00:34:01 +0800

    rename to account

--- app/screens/navigation_screen.rb
+++ app/screens/navigation_screen.rb
@@ -37,11 +37,11 @@ class NavigationScreen < PM::TableScreen
   end
 
   def sign_in_button
-    open SignInScreen.new(nav_bar: true)
+    open Account::SignInScreen.new(nav_bar: true)
   end
 
   def sign_up_button
-    open SignUpScreen.new(nav_bar: true)
+    open Account::SignUpScreen.new(nav_bar: true)
   end
 
   def sign_out_button
